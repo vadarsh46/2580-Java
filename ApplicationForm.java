0aa578aadcfbbb3ff7c8e3e0857ee969601b0ab1
VS Adarsh <139970619+vadarsh46@users.noreply.github.com>
2025-06-19 13:41:00 +0530

    ApplicationForm.java

---
+++ ApplicationForm.java
@@ -0,0 +1,48 @@
+public class ApplicationForm {
+    public static void main(String[] args) {
+        //Personal Details
+        String lastName="Vanguri";
+        String firstName="Sai";
+        String middleName="Adarsh";
+        String address="Vizag";
+        String dob="04-03-2003";
+        String birthplace="Vizag";
+        String sex="Male";
+        String civilstatus="Single";
+        long contact= 7569794998l;
+        String Religion="Hindu";
+        
+        //family Details
+        String fathername="Ganesh";
+        String occupation="Bussiness";
+        long contactno=7858799271l;
+        String mothername="Mallika";
+        String occup="Hosewife";
+        long contno=7858799571l;
+
+        //Education
+        String collegename="Sri Chaitanya Jr College";
+        int yeargraduated=2025;
+        float Average=8.8f;
+
+        //Output
+        System.out.println("________APPLICATION FOR COLLEGE ADMISSION TEST________:\n");
+        System.out.println("Last Name:"+lastName);
+        System.out.println("First Name:"+firstName);
+        System.out.println("Middle Name:"+middleName);
+        System.out.println("Address:"+address);
+        System.out.println("Date of Birth:"+dob);
+        System.out.println("Birth Place:"+birthplace);
+        System.out.println("Sex:"+sex);
+        System.out.println("Status:"+civilstatus);
+        System.out.println("Contact Number:"+contact);
+        System.out.println("Religion:"+Religion);
+        System.out.println("Father Name:"+fathername);
+        System.out.println("Occupation:"+occupation);
+        System.out.println("Contact  Number:"+contactno);
+        System.out.println("Mother Name:"+mothername);
+        System.out.println("Occupation:"+occup);
+        System.out.println("Contact Number:"+contactno);
+    }   
+
+}
