12cbd3f311298c5f9297879ebe3b8c9a208e70a5
VS Adarsh <139970619+vadarsh46@users.noreply.github.com>
2025-06-19 12:06:30 +0530

    Myntra.java

---
+++ Myntra.java
@@ -0,0 +1,28 @@
+public class Myntra {
+    public static void main(String[] args) {
+        String brand="Highlander";
+        String Des="Men Comfort relaxed fit collar casual shirt";
+        Float ratingoutof5=4.6f;
+        int Offerprice=750;
+        float discount=81.00f;
+        int mrp=3949;
+        //size
+        Short sizeS=39;
+        Short sizeM=40;
+        Short sizeL=42;
+        Short sizeXL=44;
+
+        System.out.println("===============================");
+        System.out.println("Brand:"+brand);
+        System.out.println("Description:"+Des);
+        System.out.println("Rating:"+ratingoutof5);
+        System.out.println("===============================");
+        System.out.println("OfferPrice:"+Offerprice);
+        System.out.println("Discount:"+discount);
+        System.out.println("Maximum Retail Price(MRP):"+mrp);
+        System.out.println("===============================");
+        System.out.println("Available Sizes:"+sizeS + "," +sizeM + "," +sizeL + "," +sizeXL);
+        System.out.println("===============================");
+    }   
+
+}
