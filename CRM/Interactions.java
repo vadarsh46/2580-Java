835d3660b97b4b5bd78f0e40fea26fdadb00abc5
VS Adarsh <139970619+vadarsh46@users.noreply.github.com>
2025-06-20 09:43:53 +0530

    CRM/Interactions.java

---
+++ CRM/Interactions.java
@@ -0,0 +1,20 @@
+public class Interactions {
+    public static void main(String[] args) {
+        String interactionId = "INT2001";
+        String interactionCustomerId = "customerid-45453";
+        String type = "Meetings";
+        String summary = "Discussed about project requirements";
+        String interactionDate = "20/06/2025";
+        String handledBy = "Adarsh";
+
+        //Output
+        
+        System.out.println("\n=== Interaction ===");
+        System.out.println("Interaction ID: " + interactionId);
+        System.out.println("Customer ID: " + interactionCustomerId);
+        System.out.println("Type: " + type);
+        System.out.println("Summary: " + summary);
+        System.out.println("interactionDate: " + interactionDate);
+        System.out.println("Handled By: " + handledBy);
+    }
+}
