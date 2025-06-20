5c616db3ec85c74f574a186770da5e9e60f96b8a
VS Adarsh <139970619+vadarsh46@users.noreply.github.com>
2025-06-20 09:34:07 +0530

    CRM/Customerdetails.java

--- CRM/Customerdetails.java
+++ CRM/Customerdetails.java
@@ -1,10 +1,20 @@
-package CRM;
-
-
-public class Customerdetails {
+public class customerdetails {
     public static void main(String[] args) {
         String custname="Adarsh";
+        String custemail="adarsh@gmail.com";
+        long contno=875697418l;
+        int customerId = 456780;
+        String address = "vizag";
+        String registrationDate = "20/06/2025";
 
+
+        //Output
+        System.out.println("Customer Name "+custname);
+        System.out.println("Customer Email "+custemail);
+        System.out.println("Contact Number "+contno);
+        System.out.println("Customer ID "+customerId);
+        System.out.println("Address "+address);
+        System.out.println("Registation Date "+registrationDate);
     }
     
 }
