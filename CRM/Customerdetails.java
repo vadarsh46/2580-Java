8bb6627b57936a3aed99339ac1813df44eabfbb4
VS Adarsh <saiadarsh18@gmail.com>
2025-08-04 19:25:14 +0530

    CRM/Customerdetails.java

--- CRM/Customerdetails.java
+++ CRM/Customerdetails.java
@@ -1,4 +1,4 @@
-public class customerdetails {
+public class Customerdetails {
     public static void main(String[] args) {
         String custname="Adarsh";
         String custemail="adarsh@gmail.com";
